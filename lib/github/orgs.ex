288cf35f28a93b468f82ebcb8c3de23ccb7f06c4
exAspArk <exaspark@gmail.com>
2018-08-31 14:33:19 -0400

    Add pagination options

--- lib/github/orgs.ex
+++ lib/github/orgs.ex
@@ -1,19 +1,26 @@
 defmodule Github.Orgs do
   import Github
 
+  @user_list_default_options %{
+    page: 1,
+    per_page: 30
+  }
+
   @doc """
   ## Example
 
-      iex> Github.Orgs.user_list!("access_token")
+      iex> Github.Orgs.user_list!("access_token", page: 1, per_page: 30)
       %Github.Response{
         body: [%{"name" => "WorkflowCI", ...}],
         status: 200,
         headers: [...]
       }
   """
-  def user_list!(access_token) do
+  def user_list!(access_token, options \\ []) do
+    opts = Enum.into(options, @user_list_default_options)
+
     get!(
-      "https://api.github.com/user/orgs",
+      "https://api.github.com/user/orgs?page=#{opts.page}&per_page=#{opts.per_page}",
       [{"Authorization", "token #{access_token}"}]
     ) |> to_github_response
   end
--- lib/github/users/emails.ex
+++ lib/github/users/emails.ex
@@ -1,20 +1,26 @@
 defmodule Github.Users.Emails do
   import Github
 
+  @list_default_options %{
+    page: 1,
+    per_page: 30
+  }
+
   @doc """
   ## Example
 
-      iex> Github.Users.Emails.list!("access_token")
+      iex> Github.Users.Emails.list!("access_token", page: 1, per_page: 30)
       %Github.Response{
         body: [%{"email" => "email@example.com", "primary" => true, "verified" => true, "visibility" => "private"}],
         status: 200,
         headers: [...]
       }
   """
+  def list!(access_token, options \\ []) do
+    opts = Enum.into(options, @list_default_options)
 
-  def list!(access_token) do
     get!(
-      "https://api.github.com/user/emails",
+      "https://api.github.com/user/emails?page=#{opts.page}&per_page=#{opts.per_page}",
       [{"Authorization", "token #{access_token}"}]
     ) |> to_github_response
   end
