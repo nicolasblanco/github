1c20ee21063f5fc09d5d7d84a636b22b2e6d124a
exAspArk <exaspark@gmail.com>
2018-12-23 23:49:22 -0500

    Add Github.Issues.Comments.create!/2

---
+++ lib/github/issues/comments.ex
@@ -0,0 +1,35 @@
+defmodule Github.Issues.Comments do
+  import Github.Client
+
+  @moduledoc """
+    Issues [Comments](https://developer.github.com/v3/issues/comments/).
+  """
+
+  @doc """
+  Create a comment
+
+  ## Example
+
+      iex> client = %Github.Client{access_token: "access_token"}
+
+      iex> client |> Github.Issues.Comments.create!(
+        repo_path: "WorkflowCI/github",
+        issue_number: 1,
+        body: "hello world"
+      )
+      %Github.Client.Response{
+        body: %{"url" => "https://api.github.com/repos/WorkflowCI/github/issues/comments/449685608", ...},
+        status: 201,
+        ...
+      }
+  """
+  def create!(github_client, options) do
+    opts = Enum.into(options, %{})
+
+    url = "https://api.github.com/repos/#{opts.repo_path}/issues/#{opts.issue_number}/comments"
+    body = to_json!(%{body: opts.body})
+    headers = [{"Authorization", "token #{github_client.access_token}"}]
+
+    post!(url, body, headers)
+  end
+end
---
+++ test/github/issues/comments_test.exs
@@ -0,0 +1,25 @@
+defmodule Github.Issues.CommentsTest do
+  use ExUnit.Case, async: true
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+
+  describe "create!/2" do
+    test "create a new comment" do
+      use_cassette "issues/comments.create!" do
+        response =
+          %Github.Client{access_token: "access_token"}
+          |> Github.Issues.Comments.create!(
+            repo_path: "WorkflowCI/github",
+            issue_number: 1,
+            body: "hello world"
+          )
+
+        assert response.status == 201
+        assert response.body["body"] == "hello world"
+        assert response.body["user"]["login"] == "workflowci[bot]"
+
+        assert response.body["url"] ==
+                 "https://api.github.com/repos/WorkflowCI/github/issues/comments/449685608"
+      end
+    end
+  end
+end
